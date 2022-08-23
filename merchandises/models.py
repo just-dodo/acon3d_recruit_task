581ad198924d8fc49174357641d471a57e282032
Dohyeon Park <dodo4114@naver.com>
2022-08-23 17:34:53 +0000

    Initialize Merches models

--- merchandises/models.py
+++ merchandises/models.py
@@ -1,3 +1,33 @@
+from unicodedata import name
 from django.db import models
+from core.common_models import TimeModel
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Merchandise(TimeModel):
+    name = models.CharField(max_length=500)
+    author = models.ForeignKey(
+        User, related_name="merchandises", on_delete=models.SET_NULL, null=True
+    )
+    released_at = models.DateTimeField(null=True, default=None)
+    is_reviewed = models.BooleanField(default=False)
+    commission_rate = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class MerchContent(TimeModel):
+    merchendise = models.ForeignKey(
+        Merchandise, related_name="contents", on_delete=models.CASCADE
+    )
+
+    LanguageChoices = (
+        ("ko", "Korean"),
+        ("en", "English"),
+        ("zh", "Chinese"),
+    )
+    language = models.CharField(
+        max_length=10, choices=LanguageChoices, default="Korean"
+    )
+
+    title = models.CharField(max_length=100)
+    body = models.TextField(max_length=10000, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
