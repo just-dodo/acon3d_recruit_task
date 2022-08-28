41ac770ce2ddfd476807ccb2f4ea27dc75fcdf23
Dohyeon Park <dodo4114@naver.com>
2022-08-28 16:17:25 +0000

    Give default groups their permissions

--- merchandises/apps.py
+++ merchandises/apps.py
@@ -10,8 +10,39 @@ def create_groups():
             print(f"The {group} group is created")
 
 
+def set_group_permission():
+
+    from django.contrib.auth.models import Group, Permission
+    from django.contrib.contenttypes.models import ContentType
+    from .models import Merchandise, MerchContent
+
+    authors_group = Group.objects.get(name="Authors")
+    editors_group = Group.objects.get(name="Editors")
+    users_group = Group.objects.get(name="Customers")
+
+    # print(Permission.objects.all())
+    merchandise_content_type = ContentType.objects.get_for_model(Merchandise)
+    merchcontent_content_type = ContentType.objects.get_for_model(MerchContent)
+
+    merch_permissions = Permission.objects.filter(
+        content_type=merchandise_content_type,
+    )
+    merchcontent_permissions = Permission.objects.filter(
+        content_type=merchcontent_content_type,
+    )
+
+    add_merch_permission = merch_permissions.get(codename="add_merchandise")
+    add_merchcontent_permission = merchcontent_permissions.get(
+        codename="add_merchcontent")
+    authors_group.permissions.add(add_merch_permission)
+    authors_group.permissions.add(add_merchcontent_permission)
+
+    editors_group.permissions.set(
+        list(merch_permissions)+list(merchcontent_permissions))
+
 def merch_post_migration(sender, **kwargs):
     create_groups()
+    set_group_permission()
 
 class MerchandisesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
