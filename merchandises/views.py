945e276e72ae7fb4ae3e54a742379a0f40e81f44
Dohyeon Park <dodo4114@naver.com>
2022-08-30 04:45:41 +0000

    Apply query params to filter

--- merchandises/views.py
+++ merchandises/views.py
@@ -1,13 +1,26 @@
 from .models import Merchandise, MerchContent
 from rest_framework import viewsets
 
-from .serializers import MerchContentSerializer, MerchandiseSerializer, PurchaseSerializer
-from django.http import response, HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest
+from .serializers import (
+    MerchContentSerializer,
+    MerchandiseSerializer,
+    PurchaseSerializer,
+)
+from django.http import (
+    response,
+    HttpResponseNotFound,
+    HttpResponseForbidden,
+    HttpResponseBadRequest,
+)
 from django.db.models import query, Prefetch, Q
 
 from django.db import transaction
 
-from rest_framework.permissions import AllowAny, IsAuthenticated, DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    DjangoModelPermissionsOrAnonReadOnly,
+)
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
@@ -25,22 +38,30 @@ class MerchandiseViewSet(viewsets.GenericViewSet):
     def get_queryset(self):
 
         merch_kwargs = {}
-        if self.request.user.is_anonymous or not self.request.user.groups.filter(name="Editors").exists():
+        if (
+            self.request.user.is_anonymous
+            or not self.request.user.groups.filter(name="Editors").exists()
+        ):
             merch_kwargs["is_reviewed"] = True
 
+        query_params = self.request.query_params.dict()
         contents_kwargs = {}
-        language = self.request.query_params.get("language", "")
+        language = query_params.pop("language", "")
+
         if language:
-            contents_kwargs['language'] = language
+            contents_kwargs["language"] = language
+        merch_kwargs.update(query_params)
+
         queryset = Merchandise.objects.filter(
-            deleted_at__isnull=True, **merch_kwargs).prefetch_related(
-            Prefetch("contents",
-                     queryset=MerchContent.objects.filter(
-                         deleted_at__isnull=True,
-                         **contents_kwargs
-                     ),
-                     to_attr="merchandise_contents",
-                     )
+            deleted_at__isnull=True, **merch_kwargs
+        ).prefetch_related(
+            Prefetch(
+                "contents",
+                queryset=MerchContent.objects.filter(
+                    deleted_at__isnull=True, **contents_kwargs
+                ),
+                to_attr="merchandise_contents",
+            )
         )
         return queryset
 
@@ -50,10 +71,10 @@ class MerchandiseViewSet(viewsets.GenericViewSet):
         data = request.data
 
         serializer = self.get_serializer_class()(
-            data=data, context={'request': request})
+            data=data, context={"request": request}
+        )
         serializer.is_valid(raise_exception=True)
         merchandise = serializer.save(author=user)
-
 
         image = request.FILES.get("image", None)
         if image:
@@ -78,11 +99,17 @@ class MerchandiseViewSet(viewsets.GenericViewSet):
         except Merchandise.DoesNotExist:
             return HttpResponseNotFound("The merchandise does not exist")
 
-        if merchandise.author != user and not user.groups.filter(name="Editors").exists():
-            return HttpResponseForbidden("You are neither the author of this merchandise nor a editor")
+        if (
+            merchandise.author != user
+            and not user.groups.filter(name="Editors").exists()
+        ):
+            return HttpResponseForbidden(
+                "You are neither the author of this merchandise nor a editor"
+            )
 
         serializer = self.get_serializer_class()(
-            merchandise, data=data, partial=True, context={'request': request})
+            merchandise, data=data, partial=True, context={"request": request}
+        )
         serializer.is_valid(raise_exception=True)
         merchandise = serializer.save()
 
@@ -110,7 +137,6 @@ class MerchandiseViewSet(viewsets.GenericViewSet):
         data = serializer.data
         return Response(data)
 
-
     @action(detail=True, methods=["POST"])
     def purchase(self, request, pk):
         user = request.user
@@ -122,17 +148,22 @@ class MerchandiseViewSet(viewsets.GenericViewSet):
         except Merchandise.DoesNotExist:
             return HttpResponseNotFound("The merchandise does not exist")
 
-        if merchandise.author == user :
-            return HttpResponseBadRequest("You cannot buy your merchandise for yourself.")
-        
+        if merchandise.author == user:
+            return HttpResponseBadRequest(
+                "You cannot buy your merchandise for yourself."
+            )
 
         serialize_data = {
-            'merchandise' : merchandise.id,
-            'user' : user.id,
+            "merchandise": merchandise.id,
+            "user": user.id,
         }
 
-        serializer = PurchaseSerializer(data=serialize_data,partial=True, context={'request':request,'merchandise':merchandise})
+        serializer = PurchaseSerializer(
+            data=serialize_data,
+            partial=True,
+            context={"request": request, "merchandise": merchandise},
+        )
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        
+
         return Response(serializer.data)
